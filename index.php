ac6ef48c86bb47ff6d19d476932658fab280609c
Bilal Ajaha <bilalajaha@gmail.com>
2024-11-21 13:07:27 +0100

    Firts commmit as php file

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <div>
+            <h1>Send us a message</h1>
+
+            <fieldset>
+                <div>
+                    <input type="text" id="name" name="name" placeholder="Enter your name">
+                    <i class="bi bi-person-fill"></i>
+                </div>
+
+                <div>
+                    <input type="text" id="name" name="name" placeholder="Enter your name">
+                    <i class="bi bi-envelope-fill"></i>
+                </div>
+            </fieldset>
+            
+            <fieldset>
+                <div>
+                    <input type="text" id="name" name="name" placeholder="Enter your name">
+                    <i class="bi bi-telephone-fill"></i>
+                </div>
+
+                <div>
+                    <input type="text" id="name" name="name" placeholder="Enter your name">
+                    <i class="bi bi-person-fill"></i>
+                </div>
+            </fieldset>
+
+            <fieldset>
+                <div>
+                    <input type="text" id="name" name="name" placeholder="Enter your name">
+                    <i class="bi bi-person-fill"></i>
+                </div>
+
+                <div>
+                    <input type="text" id="name" name="name" placeholder="Enter your name">
+                    <i class="bi bi-envelope-fill"></i>
+                </div>
+            </fieldset>
+                
+            
+        </div>
+    </section>
+</body>
+</html>
